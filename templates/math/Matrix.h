de93604b1cdeb0fbc64b817b2d716b379b996cac
mzhang2021 <mzhangschool2@gmail.com>
2021-12-11 19:12:03 -0500

    added some math

---
+++ templates/math/Matrix.h
@@ -0,0 +1,73 @@
+/**
+ * Description: Matrix class that supports matrix operations.
+ * Author: smax
+ * Source: own
+ * Verification: https://www.hackerearth.com/problem/algorithm/pk-and-interesting-language/description/
+ * Time: O(n^2) addition and subtraction, O(n^3) multiplication
+ */
+
+template<typename T>
+struct Matrix {
+    int n, m;
+    vector<vector<T>> mat;
+
+    Matrix(int _n, int _m) : n(_n), m(_m), mat(n, vector<T>(m)) {}
+
+    Matrix(const vector<vector<T>> &_mat) : n((int) _mat.size()), m((int) _mat[0].size()), mat(_mat) {}
+
+    Matrix& operator += (const Matrix &other) {
+        for (int i=0; i<n; i++)
+            for (int j=0; j<m; j++)
+                mat[i][j] += other.mat[i][j];
+        return *this;
+    }
+
+    Matrix& operator -= (const Matrix &other) {
+        for (int i=0; i<n; i++)
+            for (int j=0; j<m; j++)
+                mat[i][j] -= other.mat[i][j];
+        return *this;
+    }
+
+    Matrix operator * (const Matrix &other) const {
+        Matrix ret(n, other.m);
+        for (int i=0; i<n; i++)
+            for (int k=0; k<m; k++)
+                for (int j=0; j<other.m; j++)
+                    ret.mat[i][j] += mat[i][k] * other.mat[k][j];
+        return ret;
+    }
+
+    friend Matrix operator + (Matrix a, const Matrix &b) {
+        return a += b;
+    }
+
+    friend Matrix operator - (Matrix a, const Matrix &b) {
+        return a -= b;
+    }
+
+    Matrix& operator *= (const Matrix &other) {
+        return *this = *this * other;
+    }
+
+    vector<T>& operator [] (int i) {
+        return mat[i];
+    }
+
+    friend Matrix power(Matrix a, long long b) {
+        Matrix ret(a.n, a.m);
+        for (int i=0; i<a.n; i++)
+            ret[i][i] = 1;
+        while (b > 0) {
+            if (b & 1)
+                ret *= a;
+            a *= a;
+            b >>= 1;
+        }
+        return ret;
+    }
+
+    friend ostream& operator << (ostream &os, const Matrix &a) {
+        return os << a.mat;
+    }
+};
---
+++ templates/math/ModInt.h
@@ -0,0 +1,28 @@
+/**
+ * Description: Mod integer class for doing modular arithmetic.
+ * Author: smax
+ * Source: https://github.com/jakobkogler/Algorithm-DataStructures/blob/master/Math/Modular.h
+ * Verification: https://open.kattis.com/problems/modulararithmetic
+ * Time: O(log MOD) for division, O(1) operations for other operations
+ */
+
+template<int MOD>
+struct ModInt {
+    long long v;
+    ModInt(long long _v = 0) {v = (-MOD < _v && _v < MOD) ? _v : _v % MOD; if (v < 0) v += MOD;}
+    ModInt& operator += (const ModInt &other) {v += other.v; if (v >= MOD) v -= MOD; return *this;}
+    ModInt& operator -= (const ModInt &other) {v -= other.v; if (v < 0) v += MOD; return *this;}
+    ModInt& operator *= (const ModInt &other) {v = v * other.v % MOD; return *this;}
+    ModInt& operator /= (const ModInt &other) {return *this *= inverse(other);}
+    bool operator == (const ModInt &other) const {return v == other.v;}
+    bool operator != (const ModInt &other) const {return v != other.v;}
+    friend ModInt operator + (ModInt a, const ModInt &b) {return a += b;}
+    friend ModInt operator - (ModInt a, const ModInt &b) {return a -= b;}
+    friend ModInt operator * (ModInt a, const ModInt &b) {return a *= b;}
+    friend ModInt operator / (ModInt a, const ModInt &b) {return a /= b;}
+    friend ModInt operator - (const ModInt &a) {return 0 - a;}
+    friend ModInt power(ModInt a, long long b) {ModInt ret(1); while (b > 0) {if (b & 1) ret *= a; a *= a; b >>= 1;} return ret;}
+    friend ModInt inverse(ModInt a) {return power(a, MOD - 2);}
+    friend istream& operator >> (istream &is, ModInt &m) {is >> m.v; m.v = (-MOD < m.v && m.v < MOD) ? m.v : m.v % MOD; if (m.v < 0) m.v += MOD; return is;}
+    friend ostream& operator << (ostream &os, const ModInt &m) {return os << m.v;}
+};
